2452256cab007f56e90482bc33bb392c1b3970f0
Aze <152387619+Azeget@users.noreply.github.com>
2024-03-04 22:46:58 +0300

    Update Online.py

--- Online.py
+++ Online.py
@@ -1,20 +1,22 @@
 from duty.objects import dp, MySignalEvent
 from duty.api_utils import set_online_privacy
 
-@dp.longpoll_event_register('+онлайн')
-@dp.my_signal_event_register('+онлайн')
+@dp.longpoll_event_register('+вечныйонлайн')
+@dp.my_signal_event_register('+вечныйонлайн')
 def set_eternal_online(event: MySignalEvent):
-    if set_online_privacy(event.db, 'all'):  # Устанавливаем вечный онлайн статус
+    result = set_online_privacy(event.db, 'all')
+    if 'response' in result and 'category' in result['response'] and result['response']['category'] == 'all':
         msg = '🌟 Вечный онлайн активирован'
     else:
         msg = '🦊 Произошла ошибка'
     event.msg_op(2, msg)
     return "ok"
 
-@dp.longpoll_event_register('-онлайн')
-@dp.my_signal_event_register('-онлайн')
+@dp.longpoll_event_register('-вечныйонлайн')
+@dp.my_signal_event_register('-вечныйонлайн')
 def unset_eternal_online(event: MySignalEvent):
-    if set_online_privacy(event.db, 'none'):  # Отключаем вечный онлайн статус
+    result = set_online_privacy(event.db, 'none')
+    if 'response' in result and 'category' in result['response'] and result['response']['category'] == 'none':
         msg = '🙁 Вечный онлайн деактивирован'
     else:
         msg = '🦊 Произошла ошибка'
